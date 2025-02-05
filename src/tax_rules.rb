693abb8bd22e3b71234c33ebd9616230fc2a964d
boapeca <genesiistv@gmail.com>
2025-02-05 19:25:33 +0000

    implemented services logic

--- src/tax_rules.rb
+++ src/tax_rules.rb
@@ -27,4 +27,22 @@ class TaxRules
     end
   end
 
+  def self.apply_digital_service_tax(transaction)
+    if transaction.buyer_location == 'Spain'
+      { tax: transaction.price * 0.21, type: 'VAT' }
+    elsif EU_COUNTRIES.include?(transaction.buyer_location)
+      if transaction.buyer_type == 'individual'
+        vat_rate = COUNTRY_VAT_RATES[transaction.buyer_location] || 0
+        { tax: transaction.price * (vat_rate / 100.0), type: 'VAT' }
+      else
+        { tax: 0, type: 'reverse charge' }
+      end
+    else
+      { tax: 0, type: 'no tax' }
+    end
+  end
+
+  def self.apply_onsite_service_tax(transaction)
+      { tax: transaction.price * 0.21, type: 'VAT' }
+  end
 end
